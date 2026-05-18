78777a7e5ab97fc82f42c4d001ab23ffb795f2a6
ILYSHKATRAXCODE <ilia.mischenko36@gmail.com>
2026-05-19 01:13:24 +0300

    Update main.py

--- chess_app/main.py
+++ chess_app/main.py
@@ -1,6 +1,7 @@
 import tkinter as tk
 from chess_analyzer.chess_app.gui.main_window import ChessApp
 
-root = tk.Tk()
-app = ChessApp(root)
-root.mainloop()
+if __name__ == "__main__":
+    root = tk.Tk()
+    app = ChessApp(root)
+    root.mainloop()
